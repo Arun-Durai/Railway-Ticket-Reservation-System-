ed224c2f606d0ae7c5126fbb30c9e27ad281c3b5
ARUN K <87371852+Arun-Durai@users.noreply.github.com>
2024-12-16 18:32:23 +0530

    Files are pushed

---
+++ RailwayReservation_CA/Admin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    class Admin
+    {
+        public string Name { get; set; }
+
+        public Admin(string name)
+        {
+            Name = name;
+        }
+
+        public void AddTrain(List<Train> trains, string trainName, string source, string destination, int availableSeats)
+        {
+            Train newTrain = new Train(trainName, source, destination, availableSeats);
+            trains.Add(newTrain);
+            Console.WriteLine($"New train {trainName} added from {source} to {destination}.");
+        }
+
+        public void ViewAllBookings(List<Ticket> allTickets)
+        {
+            Console.WriteLine("All Bookings:");
+            foreach (var ticket in allTickets)
+            {
+                ticket.DisplayTicketInfo();
+            }
+        }
+
+        public void ViewRevenue(List<Ticket> allTickets)
+        {
+            decimal totalRevenue = 0;
+            foreach (var ticket in allTickets)
+            {
+                totalRevenue += ticket.Price;
+            }
+            Console.WriteLine($"Total Revenue: {totalRevenue:C}");
+        }
+    }
---
+++ RailwayReservation_CA/Train.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Train
+{
+    public string TrainName { get; set; }
+    public string Source { get; set; }
+    public string Destination { get; set; }
+    public int AvailableSeats { get; set; }
+    public List<string> SeatTypes { get; set; }
+
+    public Train(string trainName, string source, string destination, int availableSeats)
+    {
+        TrainName = trainName;
+        Source = source;
+        Destination = destination;
+        AvailableSeats = availableSeats;
+        SeatTypes = new List<string> { "Sleeper", "First Class", "AC" };
+    }
+
+
+    public void DisplayTrainDetails()
+    {
+        Console.WriteLine($"Train: {TrainName}, From: {Source}, To: {Destination}, Available Seats: {AvailableSeats}");
+    }
+
+
+    public void UpdateAvailableSeats(string seatType, int change)
+    {
+        AvailableSeats += change;
+    }
+
+    public bool CheckAvailability()
+    {
+        return AvailableSeats > 0;
+    }
+}
